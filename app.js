e9ca67e1d8bfd3d85ebd3ce2628cf22065aff227
user1337 <nawrashamdan@outlook.com>
2021-09-17 15:54:51 +0300

    final

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.listen(port,()=>console.log('serverstarted'));
 
 let counter =1; 
 
-const myPosts = {counter:["post title","hello guys<3"]};
+let posts = [{postId:counter,title:'Lorem Ipsum',content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus sem, porttitor sed vehicula et, convallis non felis. Nunc venenatis dapibus justo, sit amet varius augue sollicitudin at. Maecenas in elit dictum, pulvinar libero a, pharetra tortor. Nam cursus faucibus ex quis dictum. Sed a eros fringilla, congue est ac, efficitur libero. Vestibulum nec pulvinar mauris, quis tincidunt ligula. Suspendisse potenti. Curabitur elementum odio ut lectus maximus, sit amet lobortis est imperdiet. Suspendisse potenti. Mauris eget tincidunt nunc, a molestie erat."}];
 
 app.get('/',(req,res)=>{
-
-    res.render('home',{myPosts:myPosts});
+    
+    res.render('home',{posts:posts});
 })
 
 app.get('/contactme',(req,res)=>{
@@ -41,6 +41,24 @@ app.get('/compose',(req,res)=>{
 
 app.post('/compose',(req,res)=>{
     counter++;
-    console.log(counter);
+    const post = {
+        postId:counter,
+        title:req.body.postTitle,
+        content:req.body.postBody
+    };
+    posts.push(post);
+    res.redirect('/');
+})
 
+app.get('/:postId',(req,res)=>{
+    let search = req.params.postId;
+    let page =[]
+    for (let index = 0; index < posts.length; index++) {
+        if (search==posts[index].postId) {
+         page = [posts[index].title, posts[index].content];
+         break;   
+        }
+    }
+    res.render('post',{page:page});
+    
 })
