49f391c8c162e985fbbf8941ad08ab6305aaad94
user1337 <nawrashamdan@outlook.com>
2021-09-20 10:08:49 +0300

    alpha2

--- app.js
+++ app.js
@@ -2,63 +2,79 @@ const express = require('express');
 const ejs = require('ejs');
 const { urlencoded, response } = require('express');
 const app = express();
-
-app.use(urlencoded({extended:true}))
+app.use(urlencoded({ extended: true }))
 app.use(express.static('public'));
-app.set('view engine','ejs');
-
+app.set('view engine', 'ejs');
 const port = 3000;
-app.listen(port,()=>console.log('serverstarted'));
+app.listen(port, () => console.log('serverstarted'));
+/////////////////////////////////////
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect("mongodb+srv://admin_user:jOnMb0dzqqpxnBqN@cluster0.rartd.mongodb.net/Blog");
+const postSchema = Schema({
+    title: {
+        type: String,
+        require: true,
+        auto: false
+    },
+    content: {
+        type: String,
+        required: true,
+        auto: false
+    }
+})
+const Post = mongoose.model('Post',postSchema,"posts");
 
-let counter =1; 
+app.get('/', (req, res) => {
 
-let posts = [{postId:counter,title:'Lorem Ipsum',content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus sem, porttitor sed vehicula et, convallis non felis. Nunc venenatis dapibus justo, sit amet varius augue sollicitudin at. Maecenas in elit dictum, pulvinar libero a, pharetra tortor. Nam cursus faucibus ex quis dictum. Sed a eros fringilla, congue est ac, efficitur libero. Vestibulum nec pulvinar mauris, quis tincidunt ligula. Suspendisse potenti. Curabitur elementum odio ut lectus maximus, sit amet lobortis est imperdiet. Suspendisse potenti. Mauris eget tincidunt nunc, a molestie erat."}];
-
-app.get('/',(req,res)=>{
-    
-    res.render('home',{posts:posts});
+    Post.find((err,data)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('home',{data,data});
+        }
+    });
 })
 
-app.get('/contactme',(req,res)=>{
+app.get('/contactme', (req, res) => {
 
     res.render('contactme');
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
 
     res.render('about');
 })
 
-
-
-app.get('/compose',(req,res)=>{
+app.get('/compose', (req, res) => {
 
     res.render('compose');
 })
 
-
-
-
-app.post('/compose',(req,res)=>{
-    counter++;
-    const post = {
-        postId:counter,
-        title:req.body.postTitle,
-        content:req.body.postBody
-    };
-    posts.push(post);
-    res.redirect('/');
-})
-
-app.get('/:postId',(req,res)=>{
-    let search = req.params.postId;
-    let page =[]
-    for (let index = 0; index < posts.length; index++) {
-        if (search==posts[index].postId) {
-         page = [posts[index].title, posts[index].content];
-         break;   
-        }
-    }
-    res.render('post',{page:page});
+app.post('/compose', (req, res) => {
     
+    const post = {
+        title:req.body.title,
+        content:req.body.content
+    }
+    Post.create(post,(err)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('home');
+        }
+    });
+
+});
+
+app.get('/:id', (req, res) => {
+    const _id = req.params.id;
+    Post.findOne({_id:_id},(err,data)=>{
+        console.log(data);
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('post', { data: data });
+        }
+    })
 })
